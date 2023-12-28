8ee880b77e63b8b3382e00d77341bc57b46e58c3
tailofleaves <tailofleaves@gmail.com>
2023-12-28 05:09:25 -0800

    added background audio setting to keep tab from sleeping

--- js/script.js
+++ js/script.js
@@ -11,9 +11,11 @@ const BING_AUTOSEARCH = {
             interval: document.getElementById("slc-interval"),
             multitab: document.getElementById("slc-multitab"),
             random: document.getElementById("slc-random"),
+            audio: document.getElementById("slc-audio"),
         },
         span: {
             progress: document.getElementById("span-progress"),
+            silence: document.getElementById("silence"),
         },
         div: {
             bing: document.getElementById("div-bing")
@@ -48,6 +50,7 @@ const BING_AUTOSEARCH = {
             let _search_interval = BING_AUTOSEARCH.localStorage.get("_search_interval");
             let _search_limit = BING_AUTOSEARCH.localStorage.get("_search_limit");
             let _randomized_intervals = BING_AUTOSEARCH.localStorage.get("_randomized_intervals");
+            let _background_audio = BING_AUTOSEARCH.localStorage.get("_background_audio");
 
             if (!_need_help.value) {
                 modal_help.show();
@@ -80,6 +83,13 @@ const BING_AUTOSEARCH = {
                 BING_AUTOSEARCH.elements.select.random.value = BING_AUTOSEARCH.search.random = (_randomized_intervals.value === "true");
             }
 
+            if (!_background_audio.value) {
+              BING_AUTOSEARCH.localStorage.set("_background_audio", BING_AUTOSEARCH.search.audio.toString());
+            }
+            else {
+                BING_AUTOSEARCH.elements.select.audio.value = BING_AUTOSEARCH.search.audio = (_background_audio.value === "true");
+            }
+
             if (!_multitab_mode.value) {
                 (function (a) {
                     if ((/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))))
@@ -99,11 +109,13 @@ const BING_AUTOSEARCH = {
           let _search_interval = BING_AUTOSEARCH.localStorage.get("_search_interval");
           let _search_limit = BING_AUTOSEARCH.localStorage.get("_search_limit");
           let _randomized_intervals = BING_AUTOSEARCH.localStorage.get("_randomized_intervals");
+          let _background_audio = BING_AUTOSEARCH.localStorage.get("_background_audio");
 
           BING_AUTOSEARCH.elements.select.interval.value = BING_AUTOSEARCH.search.interval = parseInt(_search_interval.value.toString());
           BING_AUTOSEARCH.elements.select.limit.value = BING_AUTOSEARCH.search.limit = parseInt(_search_limit.value.toString());
           BING_AUTOSEARCH.elements.select.multitab.value = BING_AUTOSEARCH.search.multitab = (_multitab_mode.value === "true");
           BING_AUTOSEARCH.elements.select.random.value = BING_AUTOSEARCH.search.random = (_randomized_intervals.value === "true");
+          BING_AUTOSEARCH.elements.select.audio.value = BING_AUTOSEARCH.search.audio = (_background_audio.value === "true");
           BING_AUTOSEARCH.elements.link.autostart.value = getAutostartLink();
       }
     },
@@ -126,6 +138,7 @@ const BING_AUTOSEARCH = {
         interval: 10000,
         multitab: false,
         random: true,
+        audio: false,
         window: {
             open: (url, window_close_delay = BING_AUTOSEARCH.interval) => {
                 try {
@@ -154,6 +167,9 @@ const BING_AUTOSEARCH = {
             }
         },
         start: () => {
+            if(BING_AUTOSEARCH.search.audio) {
+              BING_AUTOSEARCH.elements.span.silence.play();
+            }
             BING_AUTOSEARCH.elements.countdown.div.style.display = "block";
             var total_delay = 0;
             var delay_list = [];
@@ -219,6 +235,9 @@ const BING_AUTOSEARCH = {
             }
         },
         stop: () => {
+            if(BING_AUTOSEARCH.search.audio) {
+              BING_AUTOSEARCH.elements.span.silence.pause();
+            }
             window.open("https://rewards.bing.com/pointsbreakdown");
             for (let i = 0; i < timeouts.length; i++) {
               try {
@@ -258,6 +277,10 @@ const BING_AUTOSEARCH = {
 
         BING_AUTOSEARCH.elements.select.random.addEventListener("change", () => {
           BING_AUTOSEARCH.localStorage.set("_randomized_intervals", BING_AUTOSEARCH.elements.select.random.value);
+        });
+
+        BING_AUTOSEARCH.elements.select.audio.addEventListener("change", () => {
+          BING_AUTOSEARCH.localStorage.set("_background_audio", BING_AUTOSEARCH.elements.select.audio.value);
         });
 
         BING_AUTOSEARCH.elements.link.multi_test.addEventListener("click", testPopup);
@@ -320,6 +343,7 @@ function getAutostartLink() {
   url += "&limit=" + BING_AUTOSEARCH.search.limit.toString();
   url += "&interval=" + BING_AUTOSEARCH.search.interval.toString();
   url += "&randomize=" + BING_AUTOSEARCH.search.random.toString();
+  url += "&audio=" + BING_AUTOSEARCH.search.audio.toString();
   url += "&multitab=" + BING_AUTOSEARCH.search.multitab.toString();
   return url;
 }
@@ -330,7 +354,6 @@ const copyAutostartLink = async () => {
     await navigator.clipboard.writeText(element.value);
     BING_AUTOSEARCH.elements.button.copy_autostart.setAttribute('data-bs-original-title', 'Copied!');
     bootstrap.Tooltip.getInstance(BING_AUTOSEARCH.elements.button.copy_autostart).show();
-    // Optional: Display a success message to the user
   } catch (error) {
     console.error("Failed to copy to clipboard:", error);
   }
@@ -362,6 +385,12 @@ window.addEventListener("load", () => {
   if(getURLParameter('random') !== '') {
     if(getURLParameter('random') === 'true' || getURLParameter('random') === 'false') {
       BING_AUTOSEARCH.localStorage.set("_randomized_intervals", getURLParameter('random'));
+    }
+  }
+  //check for audio parameter
+  if(getURLParameter('audio') !== '') {
+    if(getURLParameter('audio') === 'true' || getURLParameter('raaudiondom') === 'false') {
+      BING_AUTOSEARCH.localStorage.set("_background_audio", getURLParameter('audio'));
     }
   }
   //check for multitab parameter
